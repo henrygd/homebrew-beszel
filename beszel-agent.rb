bd2e44b1184adc69f022849c1e831b8d1592df2e
hank <henryoflivonia@gmail.com>
2025-04-15 17:14:38 -0400

    Update beszel-agent.rb

--- beszel-agent.rb
+++ beszel-agent.rb
@@ -18,8 +18,8 @@ class BeszelAgent < Formula
         (bin/"beszel-agent-launcher").write <<~eos
           #!/bin/bash
           set -a
-          if [ -f "#{Dir.home}/.config/beszel/beszel-agent.env" ]; then
-            source "#{Dir.home}/.config/beszel/beszel-agent.env"
+          if [ -f "#{env.HOME}/.config/beszel/beszel-agent.env" ]; then
+            source "#{env.HOME}/.config/beszel/beszel-agent.env"
           fi
           set +a
           exec #{bin}/beszel-agent "$@"
@@ -36,8 +36,8 @@ class BeszelAgent < Formula
         (bin/"beszel-agent-launcher").write <<~EOS
           #!/bin/bash
           set -a
-          if [ -f "#{Dir.home}/.config/beszel/beszel-agent.env" ]; then
-            source "#{Dir.home}/.config/beszel/beszel-agent.env"
+          if [ -f "#{env.HOME}/.config/beszel/beszel-agent.env" ]; then
+            source "#{env.HOME}/.config/beszel/beszel-agent.env"
           fi
           set +a
           exec #{bin}/beszel-agent "$@"
@@ -58,8 +58,8 @@ class BeszelAgent < Formula
           (bin/"beszel-agent-launcher").write <<~EOS
             #!/bin/bash
             set -a
-            if [ -f "#{Dir.home}/.config/beszel/beszel-agent.env" ]; then
-              source "#{Dir.home}/.config/beszel/beszel-agent.env"
+            if [ -f "#{env.HOME}/.config/beszel/beszel-agent.env" ]; then
+              source "#{env.HOME}/.config/beszel/beszel-agent.env"
             fi
             set +a
             exec #{bin}/beszel-agent "$@"
@@ -78,8 +78,8 @@ class BeszelAgent < Formula
           (bin/"beszel-agent-launcher").write <<~EOS
             #!/bin/bash
             set -a
-            if [ -f "#{Dir.home}/.config/beszel/beszel-agent.env" ]; then
-              source "#{Dir.home}/.config/beszel/beszel-agent.env"
+            if [ -f "#{env.HOME}/.config/beszel/beszel-agent.env" ]; then
+              source "#{env.HOME}/.config/beszel/beszel-agent.env"
             fi
             set +a
             exec #{bin}/beszel-agent "$@"
